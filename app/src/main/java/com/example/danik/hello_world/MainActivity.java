06c95ee6a48775bf1ff014f4db3918d24ff80884
danikfedirko <danylo.fedirko@gmail.com>
2016-12-19 19:36:40 +0200

    hello world

---
+++ app/src/main/java/com/example/danik/hello_world/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.danik.hello_world;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public static void main (String args []) {
+
+        System. out. println ("Hello World");
+}
+
